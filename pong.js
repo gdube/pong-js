ca58846600812394da54395c855e00ffa1b97f9b
gdube <mr.gdube@gmail.com>
2017-08-04 21:21:31 -0400

    Fixed issue where the ball would float through the bottom of the paddles.

--- pong.js
+++ pong.js
@@ -216,16 +216,18 @@ var Game = {
 
 			// Handle Player-Ball collisions
 			if (this.ball.x - this.ball.width <= this.player.x && this.ball.x >= this.player.x - this.player.width) {
-				if (this.ball.y <= this.player.y + (this.player.height - this.ball.height) && this.ball.y >= this.player.y) {
+				if (this.ball.y <= this.player.y + this.player.height && this.ball.y >= this.player.y) {
 					beep1.play();
+					this.ball.x = (this.player.x + this.ball.width);
 					this.ball.moveX = DIRECTION.RIGHT;
 				}
 			}
 
 			// Handle paddle-ball collision
 			if (this.ball.x - this.ball.width <= this.paddle.x && this.ball.x >= this.paddle.x - this.paddle.width) {
-				if (this.ball.y <= this.paddle.y + (this.paddle.height - this.ball.height) && this.ball.y >= this.paddle.y) {
+				if (this.ball.y <= this.paddle.y + this.paddle.height && this.ball.y >= this.paddle.y) {
 					beep1.play();
+					this.ball.x = (this.paddle.x - this.ball.width);
 					this.ball.moveX = DIRECTION.LEFT;
 				}
 			}
